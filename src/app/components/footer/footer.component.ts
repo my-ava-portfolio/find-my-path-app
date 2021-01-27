c45641bf17e891d32627c023890af3457b8a8d4d
amaury5432 <amauryval@gmail.com>
2021-01-27 22:27:03 +0100

    upd (footer) content improved

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,31 +9,39 @@ import { name, dependencies } from '../../../../package.json';
 export class FooterComponent implements OnInit {
 
   author = 'amauryval';
-  authorRepoUrl = 'https://github.com/amauryval/Find-My-Path'
+  authorRepoUrl = 'https://github.com/amauryval/Find-My-Path';
   nameApp = name;
   year = 2021;
 
+  pythonVersion = '3.8';
+
+  lib1Name = 'OsmGT';
+  lib1Version = '';
+  lib1RepoUrl = 'https://github.com/amauryval/osmgt';
+
+  lib2Name = 'Flask';
+  lib2Version = '';
+  lib2RepoUrl = 'https://github.com/pallets/flask';
+
   angularVersion!: string;
-  angularRepoUrl = 'https://github.com/angular/angular'
+  angularRepoUrl = 'https://github.com/angular/angular';
 
   bootstrapVersion!: string;
-  bootstrapRepoUrl = 'https://github.com/twbs/bootstrap'
+  bootstrapRepoUrl = 'https://github.com/twbs/bootstrap';
 
   leafletVersion!: string;
-  leafletRepoUrl = 'https://github.com/Leaflet/Leaflet'
+  leafletRepoUrl = 'https://github.com/Leaflet/Leaflet';
 
   d3Version!: string;
-  d3RepoUrl = 'https://github.com/d3/d3'
+  d3RepoUrl = 'https://github.com/d3/d3';
 
   constructor() { }
 
   ngOnInit(): void {
     this.angularVersion = dependencies['@angular/core'];
-    this.bootstrapVersion = dependencies['bootstrap']
-    this.leafletVersion = dependencies['leaflet']
-    this.d3Version = dependencies['d3']
-
-
+    this.bootstrapVersion = dependencies.bootstrap;
+    this.leafletVersion = dependencies.leaflet;
+    this.d3Version = dependencies.d3;
   }
 
 }
