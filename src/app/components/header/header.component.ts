b59271d5cafc8143854499affa7491f74b567fc6
amaury5432 <amauryval@gmail.com>
2022-02-23 19:06:36 +0100

    upd (header) remove api status checker

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
-import { startWith  } from 'rxjs/operators';
 
-import { ApiStatusService } from '../../services/apistatus.service';
 import { version } from '../../../../package.json';
 
 @Component({
@@ -14,37 +11,15 @@ export class HeaderComponent implements OnInit {
   @Input() NavBarTitle = 'Nav bar title';
 
   appVersion!: string;
-  apiStatus!: string;
-  apiStatusMessage = 'Starting...';
-  ApiContinuousChecker = interval(5000); // observable which run all the time
-
-  ApiContinuousCheckerSubscription!: Subscription;
 
   constructor(
-    private ApiCheckService: ApiStatusService
   ) {
-
-    this.ApiCheckService.apiHealth.subscribe(data => {
-      this.apiStatus = data;
-      if (this.apiStatus === 'Ready') {
-        this.apiStatusMessage = this.apiStatus;
-        this.ApiContinuousCheckerSubscription.unsubscribe();
-      }
-    });
 
   }
 
   ngOnInit(): void {
     this.appVersion = version;
 
-    this.checkApiStatus();
-  }
-
-  checkApiStatus(): void {
-    this.ApiContinuousCheckerSubscription = this.ApiContinuousChecker.pipe(startWith(0)).subscribe(() => {
-        this.ApiCheckService.callApiStatus();
-      }
-    );
   }
 
 }
